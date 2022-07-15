f0d5673ce61430bf42a9e5c2410ec04ae08c2747
Patrick Nekel <nekelpatrick@gmail.com>
2022-07-14 22:46:57 -0300

    :sparkles: add header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import styles from "./Header.module.css";
+import igniteLogo from "../../assets/ignite_logo.png"
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="" />
+    </header>
+  );
+};
